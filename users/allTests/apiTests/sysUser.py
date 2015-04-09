81f184dfd085b723f9b29acb77c0ffe87c847344
Paweł Surowiec <pawel.surowiec@gmail.com>
2015-04-09 19:08:04 +0200

    - added password change API test

---
+++ users/allTests/apiTests/sysUser.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from django.core.urlresolvers import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from autofixture import AutoFixture
+
+from users.models import SysUser
+
+class APISysUserTest(APITestCase):
+    """All primary API tests"""
+
+    def test_access_user(self):
+        """Testing access to persons API values"""
+        resp = self.client.get(reverse('api:sysuser-list'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_random_user_access(self):
+        """Testing access to random created Person objects"""
+        fixture = AutoFixture(SysUser, generate_fk=True)
+        users = fixture.create(20)
+        for u in users:
+            resp = self.client.get(reverse('api:sysuser-detail', kwargs={'pk': u.id}))
+            self.assertEqual(resp.status_code, 200)
+
+    def test_changing_user_password(self):
+        """Testing user password change functionality"""
+        user, = AutoFixture(SysUser, generate_fk=True).create(1)
+        old_password = user.password
+
+        url = reverse('api:sysuser-set-password', kwargs={'pk': user.id})
+        data = {'password': 'new_password'}
+        client = APIClient()
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Server returned ' + str(response.data))
+        user = SysUser.objects.all()[0]
+        self.assertNotEqual(old_password, user.password, 'Password not changed')
--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@
 from django.test import TestCase
 import pep8
 
+ENABLE_E2E_TESTS = False
 
 # Enabling basic view tests
 from .allTests.viewTests.homePage import HomePageTest
@@ -15,14 +16,16 @@ from .allTests.classTests.personGroup import PersonGroupClassTest
 # Enabling API tests
 from .allTests.apiTests.person import APIPersonTest, APIPersonsTest_lastItems, APITest_Person_serializerSwitcher
 from .allTests.apiTests.personGroup import APIPersonGroupTest, APIPersonGroup_lastItems
+from .allTests.apiTests.sysUser import APISysUserTest
 
 # Enabling WEB tests
 # from .allTests.e2eTests.webTests import WEBTests
-from .allTests.e2eTests.personAddForm import PersonAddFormTest
-from .allTests.e2eTests.personDataChange import PersonDataChangeTest
-from .allTests.e2eTests.personCardNumberChange import PersonCardNumberChangeTest
-from .allTests.e2eTests.pgroupAddForm import PersonGroupAddFormTest
-from .allTests.e2eTests.userAddForm import UserAddFormTest
+if ENABLE_E2E_TESTS:
+    from .allTests.e2eTests.personAddForm import PersonAddFormTest
+    from .allTests.e2eTests.personDataChange import PersonDataChangeTest
+    from .allTests.e2eTests.personCardNumberChange import PersonCardNumberChangeTest
+    from .allTests.e2eTests.pgroupAddForm import PersonGroupAddFormTest
+    from .allTests.e2eTests.userAddForm import UserAddFormTest
 
 
 class PEP8Test(TestCase):
@@ -30,7 +33,7 @@ class PEP8Test(TestCase):
 
     def test_pep8(self):
         """Testing PEP8"""
-        pep8style = pep8.StyleGuide(quiet=False, ignore=['E501'])
+        pep8style = pep8.StyleGuide(quiet=False, ignore=['E501', 'E402'])
         result = pep8style.check_files(['users/admin.py', 'users/apiSerializers.py', 'users/models.py',
                                         'users/tests.py', 'users/views.py', 'users/filters.py'])
         self.assertEqual(result.total_errors, 0, 'Errors or warnings in PEP8 test')
